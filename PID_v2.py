9791939f76ffe85801f1ee9059ca10f00522b895
pyromakesmusic <tayo.castro@gmail.com>
2022-10-19 20:13:33 -0500

    should get the initial parameters to the functions that require it

--- PID_v2.py
+++ PID_v2.py
@@ -52,32 +52,38 @@ def initialize():
     Just gets some initial information from the user about the time resolution.
     :return:
     """
-    command_line = bool(input("Run in command line mode? "))
-    if command_line == False:
+    gui_mode = bool(input("Run in GUI mode? "))
+    if gui_mode == True:
         gui_mode = input("Input mode? [ipython/tkinter]")
-        gui()
-    else:
-        gui_mode = True
-    sample_rate = int(input("Sample rate in Hz (int): \n"))
-    print(sample_rate)
-    total_time = int(input("Total time in seconds (int): \n"))
-    print(total_time)
-    set_point = int(input("Set point for cruise control (int): \n"))
-    print(set_point)
-    sample_number = total_samples(sample_rate, total_time)
-    p_k = 1
-    i_k = 1
-    d_k = 1
-    scaling_factor = .01
-    return command_line, gui_mode, sample_number, sample_rate, set_point, p_k, i_k, d_k, scaling_factor
+        initialparams = gui(gui_mode)
+        print(initialparams)
 
-def gui(browser_mode=True):
+    else:
+        sample_rate = int(input("Sample rate in Hz (int): \n"))
+        print(sample_rate)
+        total_time = int(input("Total time in seconds (int): \n"))
+        print(total_time)
+        sample_number = total_samples(sample_rate, total_time)
+        print("Total number of samples:", sample_number)
+        set_point = float(input("Set point for cruise control (float): \n"))
+        print(set_point)
+        p_k = float(input("Proportional scaling factor (float):"))
+        i_k = float(input("Integral scaling factor (float): "))
+        d_k = float(input("Derivative scaling factor (float): "))
+        scaling_factor = float(input("Scaling factor for external disturbance (float): "))
+        param_list = []
+        initialparams = pd.Series(data = param_list
+        # Need to pay attention to what this returns, this is critical for initialization
+    return gui_mode, sample_number, sample_rate, set_point, p_k, i_k, d_k, scaling_factor
+
+def gui(mode):
 
     """
     Initialization Parameters
     """
+    # Two options for GUI
     # Kinematic parameters
-    if browser_mode == True:
+    if mode == "ipython":
         position_start_slider = widgets.IntSlider(
             min=0,
             max=10,
@@ -178,35 +184,53 @@ def gui(browser_mode=True):
         )
 
         # This should be a checkbox that just flips
-        control_sign = widgets.IntSlider(
+        control_sign_slider = widgets.IntSlider(
             min=0,
             max=10,
             step=1,
             description="Ctrl Sign",
             value=0
         )
-        widget_list = [position_start_slider, velocity_start_slider, accel_start_slider, t_start_slider, t_end_slider, mass_slider, scale_factor_slider, set_point_slider, p_k_slider, i_k_slider, d_k_slider, control_constant_slider, control_sign]
+        widget_list = [position_start_slider, velocity_start_slider, accel_start_slider, t_start_slider, t_end_slider,
+                       mass_slider, scale_factor_slider, set_point_slider, p_k_slider, i_k_slider, d_k_slider,
+                       control_constant_slider, control_sign_slider]
         for item in widget_list:
             display(item)
-    else:
+
+
+        pos_start = position_start_slider.value
+        vel_start = velocity_start_slider.value
+        accel_start = accel_start_slider.value
+        t_start = t_start_slider.value
+        t_end = t_end_slider.value
+        mass = mass_slider.value
+        scale_factor = scale_factor_slider.value
+        set_point = set_point_slider.value
+        p_k = p_k_slider.value
+        i_k = i_k_slider.value
+        d_k = d_k_slider.value
+        control_constant = control_constant_slider.value
+        control_sign = control_sign_slider.value
+
+    elif mode == "tkinter": # the tkinter section
         window = tk.Tk()
         window.title("PID Controller v1.a")
         frame = ttk.Frame(window, padding=10)
 
-        POS_START = tk.DoubleVar()  # meters
-        VEL_START = tk.DoubleVar()  # m/s
-        ACCEL_START = tk.DoubleVar()  # m/s^2
-        T_START = tk.IntVar()  # seconds
-        T_END = tk.IntVar()  # seconds
-        MASS = tk.DoubleVar()  # kilograms
+        pos_start = tk.DoubleVar()  # meters
+        vel_start = tk.DoubleVar()  # m/s
+        accel_start = tk.DoubleVar()  # m/s^2
+        t_start = tk.IntVar()  # seconds
+        t_end = tk.IntVar()  # seconds
+        mass = tk.DoubleVar()  # kilograms
         # PID parameters
-        SET_POINT = tk.DoubleVar()  # float(input("Set point of speed to maintain? "))
-        P_K = tk.DoubleVar()  # float(input("Proportional term? "))
-        I_K = tk.DoubleVar()  # float(input("Integral term? "))
-        D_K = tk.DoubleVar()  # float(input("Derivative term? "))
-        SCALE_FACTOR = tk.DoubleVar()  # float(input("Scaling factor for external disturbance? "))
-        CONTROL_CONSTANT = tk.DoubleVar()  # this is your k omega
-        CONTROL_SIGN = True  # this should be a checkbox
+        set_point = tk.DoubleVar()  # float(input("Set point of speed to maintain? "))
+        p_k = tk.DoubleVar()  # float(input("Proportional term? "))
+        i_k = tk.DoubleVar()  # float(input("Integral term? "))
+        d_k = tk.DoubleVar()  # float(input("Derivative term? "))
+        scale_factor = tk.DoubleVar()  # float(input("Scaling factor for external disturbance? "))
+        control_constant = tk.DoubleVar()  # this is your k omega
+        control_sign = True  # this should be a checkbox
         throttle_f = 0.0  # initial force applied by throttle = 0
         """
         Initialization Parameters
@@ -214,89 +238,96 @@ def gui(browser_mode=True):
         # Kinematic parameters
         position_start_slider = ttk.Scale(
             frame,
-            from_=10,
-            to=100,
-            orient="horizontal",
-            variable=POS_START)
+            from_ = 10,
+            to = 100,
+            orient = "horizontal",
+            variable = pos_start)
         velocity_start_slider = ttk.Scale(
             frame,
-            from_=37,
-            to=100,
-            orient="horizontal",
-            variable=VEL_START)
+            from_ = 37,
+            to = 100,
+            orient = "horizontal",
+            variable = vel_start)
         accel_start_slider = tk.Scale(
             frame,
-            from_=16,
-            to=100,
-            orient="horizontal",
-            variable=ACCEL_START)
+            from_= 16,
+            to = 100,
+            orient = "horizontal",
+            variable = accel_start)
         t_start_slider = tk.Scale(
             frame,
-            from_=1,
-            to=100,
-            orient="horizontal",
-            variable=T_START)
+            from_ = 1,
+            to = 100,
+            orient = "horizontal",
+            variable = t_start)
         t_end_slider = tk.Scale(
             frame,
-            from_=53,
-            to=100,
-            orient="horizontal",
-            variable=T_END)
+            from_ = 53,
+            to = 100,
+            orient = "horizontal",
+            variable = t_end)
         mass_slider = tk.Scale(
             frame,
-            from_=2,
-            to=100,
-            orient="horizontal",
-            variable=MASS)
+            from_ = 2,
+            to = 100,
+            orient = "horizontal",
+            variable = mass)
         scale_factor_slider = tk.Scale(
             frame,
-            from_=2,
-            to=100,
-            orient="horizontal",
-            variable=SCALE_FACTOR)
+            from_ = 2,
+            to = 100,
+            orient = "horizontal",
+            variable = scale_factor)
         # PID parameters
         set_point_slider = tk.Scale(
             frame,
-            from_=10,
-            to=100,
-            orient="horizontal",
-            variable=SET_POINT)
+            from_ = 10,
+            to = 100,
+            orient = "horizontal",
+            variable = set_point)
         p_k_slider = tk.Scale(
             frame,
-            from_=37,
-            to=100,
-            orient="horizontal",
-            variable=P_K)
+            from_ = 37,
+            to = 100,
+            orient = "horizontal",
+            variable=p_k)
         i_k_slider = tk.Scale(
             frame,
-            from_=16,
-            to=100,
+            from_ = 16,
+            to = 100,
             orient="horizontal",
-            variable=I_K)
+            variable = i_k)
         d_k_slider = tk.Scale(
             frame,
-            from_=1,
-            to=100,
-            orient="horizontal",
-            variable=D_K)
+            from_ = 1,
+            to = 100,
+            orient = "horizontal",
+            variable = d_k)
         # Control constant
         control_constant_slider = tk.Scale(
             frame,
-            from_=53,
-            to=100,
-            orient="horizontal",
-            variable=CONTROL_CONSTANT)
+            from_ = 53,
+            to = 100,
+            orient = "horizontal",
+            variable = control_constant)
         # This should be a checkbox that just flips
-        control_sign = tk.Scale(
+        control_sign_slider = tk.Scale(
             frame,
-            from_=2,
-            to=100,
-            orient="horizontal",
-            variable=MASS)
+            from_ = 2,
+            to = 100,
+            orient = "horizontal",
+            variable = control_sign)
+        widget_list = [position_start_slider, velocity_start_slider, accel_start_slider, t_start_slider, t_end_slider,
+                       mass_slider, scale_factor_slider, set_point_slider, p_k_slider, i_k_slider, d_k_slider,
+                       control_constant_slider, control_sign_slider]
+        for item in widget_list:
+            item.pack()
+        window.mainloop()
 
-        #    window.mainloop()
+    values = [pos_start, vel_start, accel_start, t_start, t_end, mass, scale_factor, set_point, p_k, i_k, d_k, control_constant, control_sign]
+    values_series = pd.Series(data = "values")
 
-    return 0
+    return values_series
 
 def noise_f(k):
     whitenoise = np.random.normal(1,2)
@@ -345,14 +376,14 @@ def mass(num_samples, df):
     mass_series = pd.Series(data = mass_list)
     df["mass"] = mass_series
     return df
-def disturbance_force(num_samples, df):
+def disturbance_force(num_samples, df, disturbance_constant):
     """
     Returns a disturbance force.
     :param num_samples:
     :param df:
     :return:
     """
-    disturbance = 1
+    disturbance = 1 * disturbance_constant
     disturbance_list = []
     for i in range(num_samples):
         disturbance_list.append(disturbance)
@@ -456,20 +487,20 @@ def pid(df, i, p_k, i_k, d_k, scaling_factor):
 
 def main():
 
-    command_line, gui_mode, total_samples, sample_freq, set_point, p_k, i_k, d_k, scaling_factor = initialize()
+    gui_mode, total_samples, sample_freq, set_point, p_k, i_k, d_k, scaling_factor = initialize()
 
     time_series = row_maker(total_samples, sample_freq)
     df = time(total_samples, sample_freq, time_series)
     df.set_index(df["time"])
 
     # Initialization stuff - this will probably be replaced later with calls to variables or GUI elements
-    df.at[0, "error"] = 0
-    df.at[0, "pid"] = 0
-    df.at[0, "throttle_force"] = 0
-    df.at[0, "total_force"] = 0
-    df.at[0, "acceleration"] = 0
-    df.at[0, "velocity"] = 0
-    df.at[0, "position"] = 0
+    df.at[0, "error"] = 0 # This should definitely still be initialized at 0
+    df.at[0, "pid"] = 0 # This should also still initialize at 0
+    df.at[0, "throttle_force"] = 0 # This should initialize at 0 except under weird circumstances
+    df.at[0, "total_force"] = 0 # This can be whatever; the disturbance force isn't necessarily zero at start
+    df.at[0, "acceleration"] = 0 # This is also not necessarily zero, it includes an initial acceleration, but can be calculated instantaneously
+    df.at[0, "velocity"] = 0 # Not necessarily 0, may be some v0
+    df.at[0, "position"] = 0 # Definitely not necessarily zero
 
 
     # Filling out the columns that we can do in one go
@@ -486,7 +517,6 @@ def main():
         df = error(set_point, df, x)
         df = pid(df, x, p_k, i_k, d_k, scaling_factor)
 
-    print(df)
     plt.plot(df["time"], df["velocity"])
     plt.show()
 
