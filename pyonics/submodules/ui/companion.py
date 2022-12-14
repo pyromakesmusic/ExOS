e45c58095e02910d18273d69c038f1b2ebc9c62a
pyromakesmusic <tayo.castro@gmail.com>
2022-12-14 01:22:23 -0600

    added companion engine (gpt-2 based)

--- pyonics/submodules/ui/companion.py
+++ pyonics/submodules/ui/companion.py
@@ -1,29 +1,30 @@
 import torch
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-model = GPT2LMHeadModel.from_pretrained('gpt2')
+class Companion:
+    def chat(self):
+        tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+        model = GPT2LMHeadModel.from_pretrained('gpt2')
 
-sequence = '''You can't smoke tonight, you get way too live
-And you can't drink tonight, you get fucked up sometimes
-You and I, I lose every fight
-We blur every line, and risk it every time
-Girl, who do you blame?
-Blame me
-I never start a fight, I man up every time
-Girl, who do you blame?
-Blame me, blame me, no, blame me
-Girl, who do you blame?
-I'm single, I'm faithful
-I'm young, and I'm able
-Real ones on my payroll
-I'm signed to my label
-She's Queen and I'm Pharaoh
-For her I'd empty my barrel
-Timbs' on, with her hair low
-Real cute, but still ghetto, I'''
+        go_flag = True
+        sequence = '''This is placeholder text that should be filled with useful operational information'''
+        inputs = tokenizer.encode(sequence, return_tensors='pt')
+        outputs = model.generate(inputs, max_length=200, do_sample=True)
+        text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        print(text)
 
-inputs = tokenizer.encode(sequence, return_tensors='pt')
-outputs = model.generate(inputs, max_length=200, do_sample=True)
-text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-print(text)
+    def __init__(self):
+        self.tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+        self.model = GPT2LMHeadModel.from_pretrained('gpt2')
+        self.go_flag = True
+
+        while self.go_flag == True:
+
+            sequence = input()
+            if sequence == "":
+                self.go_flag == False
+            else:
+                inputs = tokenizer.encode(sequence, return_tensors='pt')
+                outputs = model.generate(inputs, max_length=200, do_sample=True)
+                text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+                print(text)
