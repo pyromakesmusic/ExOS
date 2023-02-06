358b9cfa56fc5d77d165d9c16f256c0ad678bc01
pyromakesmusic <tayo.castro@gmail.com>
2023-02-05 20:36:13 -0600

    loading some kind of world xml file now

--- shoulder_joint/shoulder_test2.py
+++ shoulder_joint/shoulder_test2.py
@@ -11,8 +11,8 @@ import sys
 CONFIG
 """
 
-w_filepath = r"D:\#PERSONAL\#STEDWARDS\Klampt-examples\Python3\exercises\control\world1.xml"
-
+w_filepath = r"D:\#PERSONAL\#STEDWARDS\Math4157\shoulder_joint\4157world_1.xml"
+r_filepath = r"D:\#PERSONAL\#STEDWARDS\Math4157\shoulder_joint\urdf\shoulder_bot.urdf"
 
 
 """
@@ -23,10 +23,13 @@ class ExoTest():
     def __init__(self, world_location):
         self.world = klampt.WorldModel()
         self.world.readFile(world_location)
-        klampt.vis.show()
-        time.sleep(10)
+
+        klampt.vis.run()
+        klampt.vis.add("world", self.world)
 
 exo = ExoTest(w_filepath)
+
+
 
 """
 world = klampt.WorldModel()
