f10dd84c2ca3101cab9d1cc5d9019d105277ec4c
pyromakesmusic <pyromakesmusic@gmail.com>
2023-02-06 11:49:58 -0600

    put a cube in the world

--- shoulder_joint/shoulder.py
+++ shoulder_joint/shoulder.py
@@ -7,11 +7,12 @@ import math
 """
 FILEPATHS
 """
-
+thin_cube = r"C:\Users\Pyro\Documents\Math4157\Klampt-examples\Python3\exercises\control\thincube.off"
 
 """
 PROGRAM
 """
 world = klampt.WorldModel()
-
+floor = world.loadElement(thin_cube)
+klampt.vis.add("world",world)
 klampt.vis.run()
