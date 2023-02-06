150a0b8836c42301e37993ffe0667146096489a1
pyromakesmusic <tayo.castro@gmail.com>
2023-02-05 19:07:04 -0600

    made a world and it waits for 10 seconds

--- shoulder_joint/shoulder_test2.py
+++ shoulder_joint/shoulder_test2.py
@@ -1,8 +1,37 @@
 import klampt
 from klampt import vis
+from klampt.io import loader
 from klampt.vis import GLRealtimeProgram
 
 import time
 import math
 import sys
 
+"""
+CONFIG
+"""
+
+w_filepath = r"D:\#PERSONAL\#STEDWARDS\Klampt-examples\Python3\exercises\control\world1.xml"
+
+
+
+"""
+CLASS DEFINITIONS
+"""
+
+class ExoTest():
+    def __init__(self, world_location):
+        self.world = klampt.WorldModel()
+        self.world.readFile(world_location)
+        klampt.vis.show()
+        time.sleep(10)
+
+exo = ExoTest(w_filepath)
+
+"""
+world = klampt.WorldModel()
+world.readFile(r"D:\#PERSONAL\#STEDWARDS\Klampt-examples\Python3\exercises\control\world1.xml")
+robot = world.makeRobot("robot")
+klampt.vis.show()
+klampt.vis.lock()
+"""
