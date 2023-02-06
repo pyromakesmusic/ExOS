480fe03c81040344d0226f72bf394e93bd18c1a0
pyromakesmusic <pyromakesmusic@gmail.com>
2023-02-06 11:09:57 -0600

    trying to load elements

--- shoulder_joint/shoulder.py
+++ shoulder_joint/shoulder.py
@@ -3,10 +3,15 @@ import klampt.vis
 import numpy as np
 import math
 
+
+"""
+FILEPATHS
+"""
+
+
+"""
+PROGRAM
+"""
 world = klampt.WorldModel()
 
-world.readFile(r"C:\Users\Pyro\Documents\Math4157\Klampt-examples\data\simulation_test_worlds\jointtest.xml")
-
-klampt.vis.add("world", world)
-klampt.vis.show()
-robot = world.robot(0)
+klampt.vis.run()
