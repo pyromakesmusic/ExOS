03a770cd65d9a137484943990a52e84087532470
pyromakesmusic <tayo.castro@gmail.com>
2023-02-07 01:15:27 -0600

    object oriented is paying dividends

--- shoulder_joint/shoulder_3OOP.py
+++ shoulder_joint/shoulder_3OOP.py
@@ -2,3 +2,49 @@ import klampt
 import klampt.vis
 import klampt.model.create.moving_base_robot as kmcmbr
 import klampt.model.create.primitives as kmcp
+
+
+"""
+MATRIX DATA
+"""
+null_matrix = [[0,0,0],[0,0,0],[0,0,0]]
+null_origin = [1,1,1]
+null_imu = (null_matrix, null_origin)
+
+
+"""
+CLASS DEFINITIONS
+"""
+class ExoSim():
+    """
+    Makes a world with a green floor and gravity. This is probably going to be the framework that I build out.
+    """
+    def __init__(self):
+        self.world = klampt.WorldModel()
+        self.ulator = klampt.sim.simulation.SimpleSimulator(self.world)
+        self.ulator.setGravity([0,0,-9.8])
+        self.floor_geom = kmcp.box(50, 50, .01,center=[0,0,0])
+        self.floor = self.world.makeTerrain("floor")
+        self.floor.geometry().set(self.floor_geom)
+        self.floor.appearance().setColor(0.2,0.6,0.3,1.0)
+        klampt.vis.add("world",self.world)
+        klampt.vis.run()
+
+    def insert(self, names, entity):
+        klampt.vis.add(names, entity)
+
+    def update(self):
+        self.ulator.updateWorld()
+class ExoBot(klampt.RobotModel):
+    def __init__(self):
+        print("ExoBot Created")
+
+"""
+MAIN FUNCTION CALL
+"""
+
+exo_test = ExoSim()
+shoulder_test = ExoBot()
+exo_test.insert(0, shoulder_test)
+while True:
+    exo_test.update()
