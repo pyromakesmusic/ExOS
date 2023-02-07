34b2ebf9b00884c167a30d91ce25a54ef04f3c03
pyromakesmusic <tayo.castro@gmail.com>
2023-02-07 00:54:39 -0600

    trying to refactor to an object oriented design

--- shoulder_joint/shoulder_2.py
+++ shoulder_joint/shoulder_2.py
@@ -3,36 +3,43 @@ import klampt.vis
 import klampt.model.create.primitives as kmcp
 import klampt.model.create.planar_robot as kmcpr
 import klampt.model.create.moving_base_robot as kmcmbr
+
+"""
+MATRIX DATA
+"""
+null_matrix = [[0,0,0],[0,0,0],[0,0,0]]
+null_origin = [1,1,1]
+null_imu = (null_matrix, null_origin)
+
+
+"""
+CLASS DEFINITIONS
+"""
+class ExoSim():
+    def __init__(self):
+        self.world = WorldModel()
+        self.imulator = Simulator(self.world)
+        self.imulator.setGravity([0,0,-1])
+        klampt.vis.add("world",self.world)
+        klampt.vis.run()
+
+
 """
 FUNCTION DEFINITIONS
 """
 KLAMPT_EXAMPLES = 'Klampt-examples'
 
-def make_shelf(world,width,depth,height,wall_thickness=0.005):
+def make_floor(world,width,depth,wall_thickness=0.005):
     """Makes a new axis-aligned "shelf" centered at the origin with
     dimensions width x depth x height. Walls have thickness wall_thickness.
     """
-    left = Geometry3D()
-    right = Geometry3D()
-    back = Geometry3D()
-    bottom = Geometry3D()
-    top = Geometry3D()
-    #method 1
-    left.loadFile("terrains/block.off")
-    left.transform([wall_thickness,0,0,0,depth,0,0,0,height],[-width*0.5,-depth*0.5,0])
-    right.loadFile("terrains/block.off")
-    right.transform([wall_thickness,0,0,0,depth,0,0,0,height],[width*0.5,-depth*0.5,0])
-    #method 2
-    back.loadFile("terrains/block.off")
-    back.scale(width,wall_thickness,height)
-    back.translate([-width*0.5,depth*0.5,0])
+
     #equivalent to back.transform([width,0,0,0,wall_thickness,0,0,0,height],[-width*0.5,depth*0.5,0])
     #method 3
     bottom = kmcp.box(width,depth,wall_thickness,center=[0,0,0])
-    top = kmcp.box(width,depth,wall_thickness,center=[0,0,height-wall_thickness*0.5])
     shelfgeom = Geometry3D()
     shelfgeom.setGroup()
-    for i,elem in enumerate([left,right,back,bottom,top]):
+    for i,elem in enumerate([bottom]):
         g = Geometry3D(elem)
         shelfgeom.setElement(i,g)
     shelf = world.makeTerrain("shelf")
@@ -41,10 +48,23 @@ def make_shelf(world,width,depth,height,wall_thickness=0.005):
     return shelf
 
 w = klampt.WorldModel()
-robot = kmcpr.make(1, w)
+exo_test = klampt.sim.simulation.SimpleSimulator(w)
+exo_test.setGravity([0,0,-1])
+shoulderbot = kmcpr.make(1, w)
 
-print(robot.numLinks())
-shelf = make_shelf(w, 1,1,1)
+# This makes a flat plane.
+shelf = make_floor(w, 50,50,.01)
 
+print("shelf ID", shelf.getID())
+print("bot ID", shoulderbot.getID())
+
+overlaps = exo_test.checkObjectOverlap()
+print("overlaps", overlaps)
 klampt.vis.add("world", w)
+print("penetration test", exo_test.hadContact(0,1))
+print(w.numRobots())
+exo_test.simulate()
+print("status", exo_test.getStatus())
 klampt.vis.run()
+exo_test.updateWorld()
+klampt.vis.show()
---
+++ shoulder_joint/shoulder_3OOP.py
@@ -0,0 +1,4 @@
+import klampt
+import klampt.vis
+import klampt.model.create.moving_base_robot as kmcmbr
+import klampt.model.create.primitives as kmcp
