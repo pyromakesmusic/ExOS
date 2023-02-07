7b0583dbabf049cb8062a1c5e5b7b2f31d8b9018
pyromakesmusic <pyromakesmusic@gmail.com>
2023-02-06 20:26:58 -0600

    added a sphere to the cube

--- shoulder_joint/shoulder.py
+++ shoulder_joint/shoulder.py
@@ -1,5 +1,6 @@
 import klampt
 import klampt.vis
+import klampt.model.create as kmc
 import numpy as np
 import math
 
@@ -8,11 +9,22 @@ import math
 FILEPATHS
 """
 thin_cube = r"C:\Users\Pyro\Documents\Math4157\Klampt-examples\Python3\exercises\control\thincube.off"
-
+shoulder_bot = "/robots/shoulder_bot.urdf"
 """
 PROGRAM
 """
+
+"""
+FUNCTIONS
+"""
+
 world = klampt.WorldModel()
-floor = world.loadElement(thin_cube)
+
+world.loadElement(thin_cube)
+ball = kmc.sphere(.2)
+
+robot = klampt.RobotModel()
+
 klampt.vis.add("world",world)
+klampt.vis.add("ball", ball)
 klampt.vis.run()
