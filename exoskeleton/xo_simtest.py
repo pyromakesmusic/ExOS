9cac21d71d8ec54633811546077bc796ab893092
pyromakesmusic <pyromakesmusic@gmail.com>
2023-06-05 15:21:25 -0500

    tried adding a force

--- exoskeleton/xo_simtest.py
+++ exoskeleton/xo_simtest.py
@@ -2,10 +2,13 @@ import klampt
 import klampt.vis
 import klampt.sim.simulation
 import klampt.io
+import math
 
 #Initializing the world
 w = klampt.io.load('WorldModel', r'C:\Users\Pyro\Documents\Math4157\Klampt-examples\data\simulation_test_worlds\stiffness_test.xml')
-print("Number of IDs in the world: ", w.numIDs())
+num = w.numIDs()
+
+print("Number of IDs in the world: ", num)
 
 #Creating the simulation
 sim = klampt.Simulator(w)
@@ -19,7 +22,8 @@ klampt.vis.add("world", w)
 klampt.vis.run()
 
 while ttime < 5:
-    body = sim.body()
+    body = sim.body(num - 1) # Probably need to find a way to reference a specific body in the world
+    body.applyForceAtPoint((1,1,1),(0,0,0))
     sim.simulate(dt)
     sim.updateWorld()
     ttime += .1
