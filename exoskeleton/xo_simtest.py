1360322ae9c088070ad3ec08206c8f7d6f3d6b62
pyromakesmusic <pyromakesmusic@gmail.com>
2023-06-06 22:54:22 -0500

    created a world file and got some kind of simulation going

--- exoskeleton/xo_simtest.py
+++ exoskeleton/xo_simtest.py
@@ -5,7 +5,7 @@ import klampt.io
 import math
 
 #Initializing the world
-w = klampt.io.load('WorldModel', r'C:\Users\Pyro\Documents\Math4157\Klampt-examples\data\simulation_test_worlds\stiffness_test.xml')
+w = klampt.io.load('WorldModel', 'xosim_world_v1.xml')
 num = w.numIDs()
 
 print("Number of IDs in the world: ", num)
@@ -14,7 +14,6 @@ print("Number of IDs in the world: ", num)
 sim = klampt.Simulator(w)
 dt = .1
 sim.setGravity((0,0,-10))
-ttime = 0
 
 #Adding it to the world. No further elements can be added
 klampt.vis.add("world", w)
@@ -24,7 +23,6 @@ klampt.vis.show()
 while klampt.vis.shown(): # I'm not sure this loop is running at all
     body = sim.body(w.rigidObject(2)) # Probably need to find a way to reference a specific body in the world
     print(body)
-    body.applyForceAtPoint((1,1,1),(0,0,0))
+    body.applyForceAtPoint((1,1,0),(0,0,0))
     sim.simulate(dt)
     sim.updateWorld()
-    ttime += .1
