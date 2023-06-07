d44b39259ff14c80fdd72a7cbaf21398d839dea7
pyromakesmusic <pyromakesmusic@gmail.com>
2023-06-07 13:49:13 -0500

    added the bones as rigid objects to the world

--- exoskeleton/xo_simtest.py
+++ exoskeleton/xo_simtest.py
@@ -12,16 +12,14 @@ print("Number of IDs in the world: ", num)
 
 #Creating the simulation
 sim = klampt.Simulator(w)
-dt = .5
-sim.setGravity((0,0,-10))
+dt = 1
+sim.setGravity((0,0,-9.8))
 
 #Adding it to the world. No further elements can be added
 klampt.vis.add("world", w)
 
 klampt.vis.show()
-
 while klampt.vis.shown(): # I'm not sure this loop is running at all
     body = sim.body(w.rigidObject(0)) # Probably need to find a way to reference a specific body in the world
-    print(body)
     sim.simulate(dt)
     sim.updateWorld()
