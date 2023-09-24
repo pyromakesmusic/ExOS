ebfaabb41d0c326b4a2d38b8e08f5487784c8ffd
pyromakesmusic <pyromakesmusic@gmail.com>
2023-09-24 15:56:32 -0500

    updated main file documentation

--- exoskeleton/main.py
+++ exoskeleton/main.py
@@ -1,4 +1,10 @@
 import xos
 
-test_config = xos.configLoader("demo_config.txt") # Config currently contains filepaths of each subRobot, will probably add other stuff
+test_config = xos.configLoader("demo_config.txt")
+"""
+Currently, the configuration file contains filepaths to:
+- the robot (ROB/URDF, this one is a ROB)
+- the muscle attachment file (CSV)
+- the world file (XML)
+"""
 exo_test = xos.ExoGUI(test_config)
