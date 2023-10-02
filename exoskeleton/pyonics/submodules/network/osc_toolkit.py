db0109204b6b5ac5005147a78930379bb7376245
pyromakesmusic <pyromakesmusic@gmail.com>
2023-10-02 10:12:53 -0500

    should be fully object-oriented and ready for use

--- exoskeleton/pyonics/submodules/network/osc_toolkit.py
+++ exoskeleton/pyonics/submodules/network/osc_toolkit.py
@@ -46,29 +46,26 @@ class BlockingServer:
 
     def __init__(self, ip, port):
         self.dispatcher = Dispatcher()
-        self.dispatcher.map("/filter", print)
 
-        self.dispatcher.map("/volume", self.print_volume_handler, "Volume")
-        self.dispatcher.map("/logvolume", self.print_compute_handler, "Log volume", math.log)
+        # self.dispatcher.map("/filter", print)
+        # self.dispatcher.map("/volume", self.print_volume_handler, "Volume")
+        # self.dispatcher.map("/logvolume", self.print_compute_handler, "Log volume", math.log)
 
         self.parser = argparse.ArgumentParser()
         self.parser.add_argument("--ip", default=ip, help="The IP address to listen on")
         self.parser.add_argument("--port", type=int, default=port, help="The port to listen on")
         self.args = self.parser.parse_args()
 
-        server = osc_server.ThreadingOSCUDPServer((args.ip, args.port), self.dispatcher)
+        self.server = None
 
-        print("Serving on {}".format(server.server_address))
-        server.serve_forever()
-
-    def print_volume_handler(self, args, volume):
-        print("[{0}] ~ {1}".format(args[0], volume))
-
-    def print_compute_handler(self, args, volume):
-        try:
-            print("[{0}] ~ {1}".format(args[0], args[1](volume)))
-        except ValueError:
-            pass
+    # def print_volume_handler(self, args, volume):
+    #     print("[{0}] ~ {1}".format(args[0], volume))
+    #
+    # def print_compute_handler(self, args, volume):
+    #     try:
+    #         print("[{0}] ~ {1}".format(args[0], args[1](volume)))
+    #     except ValueError:
+    #         pass
 
     def map(self, pattern, func, *args, **kwargs):
         """
@@ -77,6 +74,11 @@ class BlockingServer:
         args: any args for the function, this may need to be args** and kwargs** - needs more research
         """
         self.dispatcher.map(pattern, func, args)
+
+    def serve_forever(self):
+        self.server = osc_server.ThreadingOSCUDPServer((self.args.ip, self.args.port), self.dispatcher)
+        print("Serving on {}".format(self.server.server_address))
+        self.server.serve_forever()
 
 if __name__ == "__main__":
     """
