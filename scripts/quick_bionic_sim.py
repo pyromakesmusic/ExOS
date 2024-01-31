c82c19621dde94c52cc746a31f41e871b13a6b1f
pyromakesmusic <pyromakesmusic@gmail.com>
2024-01-30 21:25:54 -0600

    messing with an easy script to help edit the robot

--- scripts/quick_bionic_sim.py
+++ scripts/quick_bionic_sim.py
@@ -1,3 +1,6 @@
 import exoskeleton.exos as xos
+import klampt
 
-xos.launch_standard(r"C:\Users\Pyro\Documents\Math4157\exoskeleton\config\demo_config.txt")
+w = klampt.WorldModel()
+
+w.readFile(r"C:\Users\Pyro\Documents\Math4157\exoskeleton\robots\core_bot_test4.rob")
