e4fae8148a3447e84b228fa9ccc2a34f93d01765
pyromakesmusic <pyromakesmusic@gmail.com>
2024-03-13 01:57:37 -0500

    added encryption function

--- exoskeleton/exos.py
+++ exoskeleton/exos.py
@@ -23,6 +23,7 @@ import platform  # For detecting the platform and automatically selecting the co
 import pandas as pd  # Critical, most of the data structures are pandas structures
 import asyncio  # For asynchronous OSC handling
 import os  # For listing files in directory
+
 import sys
 import logging
 import tkinter as tk
@@ -31,7 +32,11 @@ import tkinter.filedialog as filedialog
 """
 OTHER LIBRARIES
 """
-
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 """
 KLAMPT IMPORTS
 """
@@ -142,6 +147,32 @@ def configLoader(config_name):
                   }
 
         return config
+
+"""
+SECURITY
+"""
+# Function to derive a key from a password using PBKDF2
+def derive_key(password):
+    salt = os.urandom(16)
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    return kdf.derive(password), salt
+
+# Function to encrypt data using AES-256
+def encrypt_data(data, key, salt):
+    padder = padding.PKCS7(128).padder()
+    padded_data = padder.update(data) + padder.finalize()
+    iv = os.urandom(16)
+    cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+    encrypted_data = encryptor.update(padded_data) + encryptor.finalize()
+    return salt + iv + encrypted_data
+
 
 """
 CLASS DEFINITIONS
